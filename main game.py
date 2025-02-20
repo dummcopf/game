489816069c39ae5b3d4c3d71f64c738e4af80cf0
dummcopf <doribe30@gmail.com>
2025-02-20 21:08:05 +0200

    Update main game.py

--- main game.py
+++ main game.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import math
-
+import operator
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
@@ -26,31 +26,62 @@ class Enemy:
         self.rect = pygame.Rect(rect)
         print(self.rect.x,self.rect.y)
         self.color = color
-        self.grow_speed = 1
+        self.grow_speed = 0.2
         self.original_size = self.rect.size
-
-
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.w = self.rect.w
+        self.h = self.rect.h
 
 
     def grow(self):
-        self.rect.w += math.floor(2 * self.grow_speed)
-        self.rect.h += math.floor(2 * self.grow_speed)
-        self.rect.x -= math.floor(1 * self.grow_speed)
-        self.rect.y -= math.floor(1 * self.grow_speed)
+        self.h += 2 * self.grow_speed
+        self.w += 2 * self.grow_speed
+        self.x -= 1 * self.grow_speed
+        self.y -= 1 * self.grow_speed
+
+        self.rect.w = math.floor(self.w)
+        self.rect.h = math.floor(self.h)
+        self.rect.x = math.floor(self.x)
+        self.rect.y = math.floor(self.y)
+
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
-    def reset(self):
-        self.rect.size = self.original_size
-        self.rect.x = WIDTH // 2 - 25
-        self.rect.y = HEIGHT // 2 - 25
-        self.grow_speed = 1
 
+
+def math_problem(a,b):
+    is_correct = random.randint(0,1).__bool__()
+    random_number = random.randint(0,3)
+    operations_dict = {"+":operator.add, "-":operator.sub, "/":operator.truediv, "*": operator.mul}
+    type_of_equation = "+"
+    if random_number == 1:
+        type_of_equation = "+"
+    elif random_number == 2:
+        type_of_equation = "-"
+    elif random_number == 3:
+        type_of_equation = "*"
+    else:
+        type_of_equation = "/"
+    c = operations_dict[type_of_equation](a, b)
+    if not is_correct:
+        c += math.ceil(random.uniform(-c,c))
+    print(f"{a}{type_of_equation}{b}={c} (correct = {is_correct})")
+math_problem(1,2)
+
+def spawn_enemy():
+    global last_enemy_spawn_time
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),)
+    enemies.append(Enemy((random.randint(WIDTH // 2 - WIDTH // 3, WIDTH // 2 + WIDTH // 3),random.randint(HEIGHT // 2 - HEIGHT // 3, HEIGHT // 2 + HEIGHT // 3),
+                          10,
+                          10),(color[0], color[1], color[2])))
+    last_enemy_spawn_time = pygame.time.get_ticks()
 
 gun = Gun((WIDTH // 2 - 25, HEIGHT // 2 + 250, 50, 50), (255, 255, 255))
-
-enemy = Enemy((random.randint(WIDTH//2-WIDTH//4, WIDTH//2+WIDTH//4), random.randint(HEIGHT//2-HEIGHT//4, HEIGHT//2), 10, 10), (0, 255, 255))
+last_enemy_spawn_time = pygame.time.get_ticks()
+enemy_spawn_cooldown = 1.5
+enemies = []
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,22 +89,26 @@ while running:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+            for enemy in enemies:
+                if enemy.rect.collidepoint(mouse_x, mouse_y):
+                    enemies.remove(enemy)
 
-            if enemy.rect.collidepoint(mouse_x, mouse_y):
-                enemy.reset()
-                enemy = Enemy((random.randint(WIDTH // 2 - WIDTH // 4, WIDTH // 2 + WIDTH // 4),
-                               random.randint(HEIGHT // 2, HEIGHT // 2 + HEIGHT // 4),
-                               10,
-                               10),
-                              (0, 255, 255))
 
-    enemy.grow()
-    if enemy.rect.w >= WIDTH/4:
-        enemy.reset()
+
+    if pygame.time.get_ticks() - last_enemy_spawn_time >= enemy_spawn_cooldown * 1000:
+       spawn_enemy()
+
 
     screen.fill((0, 0, 0))
     gun.draw(screen)
-    enemy.draw(screen)
+    for enemy in enemies:
+        enemy.grow()
+        enemy.draw(screen)
+        math_problem(1,3)
+        if enemy.rect.w >= WIDTH / 4:
+            enemies.remove(enemy)
+
+
     pygame.display.flip()
     clock.tick(FPS)
 
