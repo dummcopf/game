e71b90efcf60bc6d7633c8638aee96879e1a3a93
dummcopf <doribe30@gmail.com>
2025-02-20 20:09:42 +0200

    Update main game.py

--- main game.py
+++ main game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -15,27 +16,27 @@ class Gun:
     def __init__(self, rect, color):
         self.rect = pygame.Rect(rect)
         self.color = color
-#ljfnjnfkjwnkwjenckj33434
+
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
 
 class Enemy:
     def __init__(self, rect, color):
+        middle = (WIDTH/2, HEIGHT/2)
         self.rect = pygame.Rect(rect)
+        print(self.rect.x,self.rect.y)
         self.color = color
-        self.grow_speed = 10
-        self.grow_counter = 0
+        self.grow_speed = 1
         self.original_size = self.rect.size
 
-    def grow(self):
-        self.grow_counter += 1
-        self.grow_speed -=0.03
-        if self.grow_counter > self.grow_speed:
-            self.rect.inflate_ip(2, 2)
-            self.rect.x -= 0.5
-            self.rect.y -= 0.5
-            self.grow_counter = 0
 
+
+
+    def grow(self):
+        self.rect.w += math.floor(2 * self.grow_speed)
+        self.rect.h += math.floor(2 * self.grow_speed)
+        self.rect.x -= math.floor(1 * self.grow_speed)
+        self.rect.y -= math.floor(1 * self.grow_speed)
 
     def draw(self, surface):
         pygame.draw.rect(surface, self.color, self.rect)
@@ -44,12 +45,12 @@ class Enemy:
         self.rect.size = self.original_size
         self.rect.x = WIDTH // 2 - 25
         self.rect.y = HEIGHT // 2 - 25
-        self.grow_counter = 0
-        self.grow_speed = 10
+        self.grow_speed = 1
 
 
 gun = Gun((WIDTH // 2 - 25, HEIGHT // 2 + 250, 50, 50), (255, 255, 255))
-enemy = Enemy((random.randint(WIDTH//2-WIDTH//4, WIDTH//2+WIDTH//4), random.randint(HEIGHT//2-WIDTH//4, HEIGHT//2+WIDTH//4), 10, 10), (0, 255, 255))
+
+enemy = Enemy((random.randint(WIDTH//2-WIDTH//4, WIDTH//2+WIDTH//4), random.randint(HEIGHT//2-HEIGHT//4, HEIGHT//2), 10, 10), (0, 255, 255))
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -60,17 +61,19 @@ while running:
 
             if enemy.rect.collidepoint(mouse_x, mouse_y):
                 enemy.reset()
-                enemy = Enemy((random.randint(0, 800), random.randint(0, 600), 10, 10), (0, 255, 255))
+                enemy = Enemy((random.randint(WIDTH // 2 - WIDTH // 4, WIDTH // 2 + WIDTH // 4),
+                               random.randint(HEIGHT // 2, HEIGHT // 2 + HEIGHT // 4),
+                               10,
+                               10),
+                              (0, 255, 255))
 
     enemy.grow()
-    print(enemy.grow_speed)
-    if enemy.grow_speed <= 0:
+    if enemy.rect.w >= WIDTH/4:
         enemy.reset()
 
     screen.fill((0, 0, 0))
     gun.draw(screen)
     enemy.draw(screen)
-
     pygame.display.flip()
     clock.tick(FPS)
 
